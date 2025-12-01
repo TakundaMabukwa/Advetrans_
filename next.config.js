0e83edfa0d65ac2f28df8fdcf03aeff8a93e704b
TakundaMabukwa <mabukwatakunda@gmail.com>
2025-12-01 02:06:58 +0200

    build optimzation

--- next.config.js
+++ next.config.js
@@ -12,17 +12,40 @@ const nextConfig = {
   },
   experimental: {
     webpackMemoryOptimizations: true,
-    optimizePackageImports: ['lucide-react', '@radix-ui/react-icons'],
+    optimizePackageImports: [
+      'lucide-react',
+      '@radix-ui/react-icons',
+      'recharts',
+      '@mui/material',
+      '@mui/x-charts',
+      'date-fns'
+    ],
   },
   webpack: (config, { dev, isServer }) => {
     config.optimization = {
       ...config.optimization,
       minimize: true,
+      moduleIds: 'deterministic',
       splitChunks: {
         chunks: 'all',
+        maxSize: 244000,
         cacheGroups: {
           default: false,
           vendors: false,
+          framework: {
+            name: 'framework',
+            test: /[\\/]node_modules[\\/](react|react-dom|next)[\\/]/,
+            priority: 40,
+            enforce: true,
+          },
+          lib: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+              return `npm.${packageName.replace('@', '')}`;
+            },
+            priority: 30,
+          },
         },
       },
     };
@@ -40,6 +63,18 @@ const nextConfig = {
       fs: false,
       net: false,
       tls: false,
+    };
+    
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'mapbox-gl': 'mapbox-gl/dist/mapbox-gl.js',
+      };
+    }
+    
+    config.module = {
+      ...config.module,
+      noParse: /mapbox-gl\.js$/,
     };
     
     return config
